3e1e887a51955a505893ab316081afa11d2b2a8e
binyaha <yahabin@gmail.com>
2017-11-22 00:57:59 +0800

    完成new&create

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -5,4 +5,26 @@ before_action :authenticate_admin
     @restaurants=Restaurant.all
   end
 
+  def new
+    @restaurant=Restaurant.new
+  end
+
+  def create
+    @restaurant=Restaurant.new(restaurant_param)
+    if @restaurant.save
+      redirect_to admin_restaurants_path
+      flash[:notice] = "restaurant was successfully created"
+    else
+      render :new
+      flash[:alert] = "restaurant was failed to create"
+    end
+
+  end
+
+  private 
+
+  def restaurant_param
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
+
 end
